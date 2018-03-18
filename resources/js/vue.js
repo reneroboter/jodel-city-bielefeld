108df54fd32789270c70c7e792a09f8c2c812007
René Backhaus <rene.backhaus@hdnet.de>
2018-03-18 17:44:56 +0100

    save before restructure with components

--- resources/js/vue.js
+++ resources/js/vue.js
@@ -1,51 +1,18 @@
-var post = Vue.component('jodel-post', {
-    props: ['post'],
-    template: '<div class="row">\n' +
-    '    <div class="col-10">\n' +
-    '        <div class="row">\n' +
-    '            <div class="col-12">\n' +
-    '                <h6 class="card-title"><i class="far fa-map-marker"></i> here • <b>{{ post.channel }}</b> • {{ post.created }}</h6>\n' +
-    '            </div>\n' +
-    '        </div>\n' +
-    '        <div class="row">\n' +
-    '            <div class="col-12">\n' +
-    '                <p class="card-text">{{ post.message }}</p>\n' +
-    '            </div>\n' +
-    '        </div>\n' +
-    '        <div class="row">\n' +
-    '            <div class="col-12">\n' +
-    '                <p></p>\n' +
-    '                <i class="far fa-comments"> </i>\n' +
-    '                <small>{{ post.comments_count }}</small>\n' +
-    '            </div>\n' +
-    '        </div>\n' +
-    '    </div>\n' +
-    '    <div class="col-2 align-self-center">\n' +
-    '        <div class="col-12">\n' +
-    '            <i class="fas fa-angle-up"></i>\n' +
-    '            {{ post.likes_count }}\n' +
-    '            <i class="fas fa-angle-down"></i>\n' +
-    '        </div>\n' +
-    '    </div>\n' +
-    '</div>'
-});
-
-var app = new Vue({
+new Vue({
     el: '#app',
     data: {
-        errors: [],
-        posts: []
+        posts: [],
     },
-    created() {
+    mounted() {
         axios.get('http://localhost')
-            .then(response => {
-                this.posts = response.data.posts
-            })
+            .then(response => this.posts = response.data.posts)
             .catch(e => {
                 this.errors.push(e)
             })
     },
-
-    components: {post: post},
-
+    filters: {
+        moment: function (date) {
+            return moment(date, 'YYYYMMDD').fromNow();
+        }
+    }
 });
